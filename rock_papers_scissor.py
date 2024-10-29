818bd1aca0e1b1e4f002b940a827771eff6893c9
jose flrian <cjfn10101@gmail.com>
2024-10-28 22:16:22 -0600

    random in rock papper or scissor

--- rock_papers_scissor.py
+++ rock_papers_scissor.py
@@ -1,12 +1,13 @@
-computer_choice = 'scissors'
-user_choice = input('Do you want rock, paper or scissors?')
+import random
+computer_choice =  random.choice(['rock','papper','scissors'])
+user_choice = input('Do you want rock, papper or scissors?')
 if(computer_choice == user_choice):
     print ('TIE')
 elif user_choice == 'rock' and computer_choice == 'scissors':
     print('WIN')
 elif user_choice == 'paper' and computer_choice == 'rock':
     print('WIN')
-elif user_choice == 'scissors' and computer_choice == 'paper':
+elif user_choice == 'scissors' and computer_choice == 'papper':
     print('WIN')
 else:
     print('you LOSE')
